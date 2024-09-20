1e1237f8b308ab11898e04695bfcda248bb297fc
Andymancruz <136440326+Andymancruz@users.noreply.github.com>
2024-09-20 14:26:52 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,7 +7,7 @@ module.exports = {
   networks: {
     swisstronik: {
       url: "https://json-rpc.testnet.swisstronik.com/",
-      accounts: [`0x8a68ff29ed0e48582f187770cd20d7896aed4a82f60f3b65385b5e0e685aa573`],
+      accounts: [``],
     },
   },
 };
